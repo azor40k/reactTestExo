db12491f608ecef84e21ce5b723835cd9f404062
azor40k <carandang.axel@gmail.com>
2021-02-02 17:19:28 +0100

    [impr] router

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -43,7 +43,7 @@ export default function App() {
           <Route exact path="/">
             <UseState />
           </Route>
-          <Route path="*">
+          <Route path="/*">
             <NoMatch />
           </Route>
         </Switch>
